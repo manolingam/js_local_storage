2a4338597a98f1924e5399bd6cc27d3d00006794
Manolingam <manolingam1996@gmail.com>
2019-04-24 17:55:05 +0530

    Updates

---
+++ precache-manifest.99a18da3a5fc1c4cdbbad27dc54bd576.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd7814c09e821bdc8134",
+    "url": "/js_local_storage/static/js/runtime~main.973134a0.js"
+  },
+  {
+    "revision": "f64b7e4902d4c595db54",
+    "url": "/js_local_storage/static/js/main.7ba839a0.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/js_local_storage/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f64b7e4902d4c595db54",
+    "url": "/js_local_storage/static/css/main.4e9766ad.chunk.css"
+  },
+  {
+    "revision": "62c5af9636df78d5f197445d7da72038",
+    "url": "/js_local_storage/index.html"
+  }
+];
